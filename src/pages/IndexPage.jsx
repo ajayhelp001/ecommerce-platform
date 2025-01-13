1aca467d1bdc27b941d04f02c0a3dac528be095d
ajayhelp001 <144900898+ajayhelp001@users.noreply.github.com>
2025-01-13 17:56:14 +0530

    project setup

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+const IndexPage = () => {
+  return (
+    <>
+      <Header/>
+      <h2>Index</h2>
+    </>
+  )
+}
+
+export default IndexPage
